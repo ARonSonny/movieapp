6637e6c1beb5a5f0efdf762903bc377d083fe7e3
Aaron Reyes <aaronreyes@Aarons-MacBook-Pro.local>
2026-01-15 19:35:00 -0600

    Starting to work on the movie app following the instructions

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+function App() {
+
+  return (
+  <main>
+    <div className="pattern" />
+      <div className="wrapper">
+      <header>
+        <h1>Find <span className="text-gradient">movies</span> you will enjoy without the hassle</h1>
+      </header>
+      </div>
+  </main>  
+  )
+}
+
+export default App
